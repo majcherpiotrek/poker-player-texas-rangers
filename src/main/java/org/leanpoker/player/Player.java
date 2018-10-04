e2153e1505877da4f65a98f7db984946d79de1f5
PaulinaMatczuk <paulina.matczuk0309@gmail.com>
2018-10-04 15:32:16 +0200

    Korrektur

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -34,190 +34,188 @@ public class Player {
         List<List<CardModel>> listsWithTheSameCards = sortCardsByRank(cardModelAll);
 
 
-        if(fullHouse(listsWithTheSameCards)){
-            return 5* (gameStateModel.getCurrent_buy_in() - playermodel.getBet()+ 2* gameStateModel.getMinimum_raise());
+        if (fullHouse(listsWithTheSameCards)) {
+            return 5 * (gameStateModel.getCurrent_buy_in() - playermodel.getBet() + 2 * gameStateModel.getMinimum_raise());
         }
-        if(gameStateModel.getCommunity_cards().size() < 3){
-            if(shouldFold(playerCard)){
+        if (gameStateModel.getCommunity_cards().size() < 3) {
+            if (shouldFold(playerCard)) {
                 return 0;
             }
-            return gameStateModel.getCurrent_buy_in() - playermodel.getBet()+ 2* gameStateModel.getMinimum_raise();
+            return gameStateModel.getCurrent_buy_in() - playermodel.getBet() + 2 * gameStateModel.getMinimum_raise();
         }
-        if(gameStateModel.getCommunity_cards().size() >= 3) {
-            if(listsWithTheSameCards.isEmpty()){
+        if (gameStateModel.getCommunity_cards().size() >= 3) {
+            if (listsWithTheSameCards.isEmpty()) {
                 return 0;
-            }
-            else{
-                return numberOfKind(listsWithTheSameCards) * (gameStateModel.getCurrent_buy_in() - playermodel.getBet()+ 2* gameStateModel.getMinimum_raise());
+            } else {
+                return numberOfKind(listsWithTheSameCards) * (gameStateModel.getCurrent_buy_in() - playermodel.getBet() + 2 * gameStateModel.getMinimum_raise());
             }
         }
 
-    	// Check if we have any figure
-    	
-    	// Check how much money we have
-    	
-    	// Check or bet minimum raise
-    	
-    	
+        // Check if we have any figure
+
+        // Check how much money we have
+
+        // Check or bet minimum raise
+
+
         return 0;
     }
-    
-    
+
+
     public static void showdown(JsonElement game) {
     }
 
 
-    public static boolean fullHouse(List<List<CardModel>> listsWithTheSameCards){
-       if(listsWithTheSameCards.size()==2){
-           if(findThreeOfAKind(listsWithTheSameCards)!=null && findTwoOfAKind(listsWithTheSameCards)!=null){
-               return true;
-           }
-           else return false;
-       }
+    public static boolean fullHouse(List<List<CardModel>> listsWithTheSameCards) {
+        if (listsWithTheSameCards.size() == 2 && findThreeOfAKind(listsWithTheSameCards) != null && findTwoOfAKind(listsWithTheSameCards) != null) {
+            return true;
+        }
+        return false;
     }
 
-    
-    public static List<List<CardModel>> sortCardsByRank(List<CardModel> cardList) {
-    	List<List<CardModel>> result = new ArrayList<>();
-    	List<String> ranksAlreadyChecked = new ArrayList();
-    	
-    	for (int i = 0; i < cardList.size(); i++) {
-    		CardModel currentCard = cardList.get(i);
-    		if (!ranksAlreadyChecked.contains(currentCard.getRank())) {
-    			
-    			ranksAlreadyChecked.add(currentCard.getRank());
-    			List<CardModel> foundFigure = new ArrayList<>();
-    			foundFigure.add(currentCard);
-    			
-        		for (int k = 0; k < cardList.size(); k++) {
-        			CardModel nextCard = cardList.get(k);
-        			
-        			if (!currentCard.equals(nextCard) && currentCard.getRank().equals(nextCard.getRank())) {
-        				foundFigure.add(nextCard);
-        			}
-        		}
-        		
-        		if (foundFigure.size() > 1) {
-    				result.add(foundFigure);
-    			}
-    		}
-    	}
-    	return result;
 
-}
-    public static int numberOfKind(List<List<CardModel>> listsWithTheSameCards){
-        if(findFourOfAKind(listsWithTheSameCards)!=null){
+    public static List<List<CardModel>> sortCardsByRank(List<CardModel> cardList) {
+        List<List<CardModel>> result = new ArrayList<>();
+        List<String> ranksAlreadyChecked = new ArrayList();
+
+        for (int i = 0; i < cardList.size(); i++) {
+            CardModel currentCard = cardList.get(i);
+            if (!ranksAlreadyChecked.contains(currentCard.getRank())) {
+
+                ranksAlreadyChecked.add(currentCard.getRank());
+                List<CardModel> foundFigure = new ArrayList<>();
+                foundFigure.add(currentCard);
+
+                for (int k = 0; k < cardList.size(); k++) {
+                    CardModel nextCard = cardList.get(k);
+
+                    if (!currentCard.equals(nextCard) && currentCard.getRank().equals(nextCard.getRank())) {
+                        foundFigure.add(nextCard);
+                    }
+                }
+
+                if (foundFigure.size() > 1) {
+                    result.add(foundFigure);
+                }
+            }
+        }
+        return result;
+
+    }
+
+    public static int numberOfKind(List<List<CardModel>> listsWithTheSameCards) {
+        if (findFourOfAKind(listsWithTheSameCards) != null) {
             return 4;
-        }else  if(findThreeOfAKind(listsWithTheSameCards)!=null){
+        } else if (findThreeOfAKind(listsWithTheSameCards) != null) {
             return 3;
         }
-        if(findTwoOfAKind(listsWithTheSameCards)!=null){
+        if (findTwoOfAKind(listsWithTheSameCards) != null) {
             return 2;
-        }
-        else {
+        } else {
             return 1;
         }
     }
-    
-    public static String findFourOfAKind(List<List<CardModel>> sortedCards ) {
-    	for(List<CardModel> cards : sortedCards) {
-    		if(cards.size() == 4) {
-    			return cards.get(0).getRank();
-    		}
-    	}
-    	return null;
+
+    public static String findFourOfAKind(List<List<CardModel>> sortedCards) {
+        for (List<CardModel> cards : sortedCards) {
+            if (cards.size() == 4) {
+                return cards.get(0).getRank();
+            }
+        }
+        return null;
     }
-    
+
     public static String findThreeOfAKind(List<List<CardModel>> sortedCards) {
-    	for(List<CardModel> cards : sortedCards) {
-    		if(cards.size() == 3) {
-    			return cards.get(0).getRank();
-    		}
-    	}
-    	return null;
+        for (List<CardModel> cards : sortedCards) {
+            if (cards.size() == 3) {
+                return cards.get(0).getRank();
+            }
+        }
+        return null;
     }
-    
+
     public static String findTwoOfAKind(List<List<CardModel>> sortedCards) {
-    	for(List<CardModel> cards : sortedCards) {
-    		if(cards.size() == 2) {
-    			return cards.get(0).getRank();
-    		}
-    	}
-    	return null;
+        for (List<CardModel> cards : sortedCards) {
+            if (cards.size() == 2) {
+                return cards.get(0).getRank();
+            }
+        }
+        return null;
     }
-    
+
     public static List<String> findTwoPairs(List<List<CardModel>> sortedCards) {
-    	int pairCounter = 0;
-    	List<String> foundRanks = new ArrayList<>();
-    	for(List<CardModel> cards : sortedCards) {
-    		if(cards.size() == 2) {
-    			pairCounter++;
-    			foundRanks.add(cards.get(0).getRank());
-    			if(pairCounter == 2) {
-        			return foundRanks;
-        		}
-    		}
-    	}
-    	return null;
+        int pairCounter = 0;
+        List<String> foundRanks = new ArrayList<>();
+        for (List<CardModel> cards : sortedCards) {
+            if (cards.size() == 2) {
+                pairCounter++;
+                foundRanks.add(cards.get(0).getRank());
+                if (pairCounter == 2) {
+                    return foundRanks;
+                }
+            }
+        }
+        return null;
     }
-    
+
     public static List<String> findThreePairs(List<List<CardModel>> sortedCards) {
-    	int pairCounter = 0;
-    	List<String> foundRanks = new ArrayList<>();
-    	for(List<CardModel> cards : sortedCards) {
-    		if(cards.size() == 2) {
-    			pairCounter++;
-    			foundRanks.add(cards.get(0).getRank());
-    			if(pairCounter == 3) {
-        			return foundRanks;
-        		}
-    		}
-    	}
-    	return null;
+        int pairCounter = 0;
+        List<String> foundRanks = new ArrayList<>();
+        for (List<CardModel> cards : sortedCards) {
+            if (cards.size() == 2) {
+                pairCounter++;
+                foundRanks.add(cards.get(0).getRank());
+                if (pairCounter == 3) {
+                    return foundRanks;
+                }
+            }
+        }
+        return null;
     }
 
     public static boolean shouldFold(List<CardModel> cardsInHand) {
-    	List<String> ranksInHand = new ArrayList<>();
-    	ranksInHand.add(cardsInHand.get(0).getRank());
-    	ranksInHand.add(cardsInHand.get(1).getRank());
-    	return ranksInHand.contains("7") && ranksInHand.contains("2");
+        List<String> ranksInHand = new ArrayList<>();
+        ranksInHand.add(cardsInHand.get(0).getRank());
+        ranksInHand.add(cardsInHand.get(1).getRank());
+        return ranksInHand.contains("7") && ranksInHand.contains("2");
     }
-    
-    public static String flush(List<CardModel> cards) {
-    	List<List<CardModel>> cardsSortedBySuits = sortCardsBySuit(cards);
-    	for (List<CardModel> sameSuitCards : cardsSortedBySuits) {
-    		if (sameSuitCards.size() == 4) {
-    			return sameSuitCards.get(0).getSuit();
-    		}
-    	}
-    	return null;
-    }
-    public static List<List<CardModel>> sortCardsBySuit(List<CardModel> cardList) {
-    	List<List<CardModel>> result = new ArrayList<>();
-    	List<String> ranksAlreadyChecked = new ArrayList();
-    	
-    	for (int i = 0; i < cardList.size(); i++) {
-    		CardModel currentCard = cardList.get(i);
-    		if (!ranksAlreadyChecked.contains(currentCard.getRank())) {
-    			
-    			ranksAlreadyChecked.add(currentCard.getRank());
-    			List<CardModel> foundFigure = new ArrayList<>();
-    			foundFigure.add(currentCard);
-    			
-        		for (int k = 0; k < cardList.size(); k++) {
-        			CardModel nextCard = cardList.get(k);
-        			
-        			if (!currentCard.equals(nextCard) && currentCard.getSuit().equals(nextCard.getSuit())) {
-        				foundFigure.add(nextCard);
-        			}
-        		}
-        		
-        		if (foundFigure.size() > 1) {
-    				result.add(foundFigure);
-    			}
-    		}
-    	}
-    	return result;
 
-}
+    public static String flush(List<CardModel> cards) {
+        List<List<CardModel>> cardsSortedBySuits = sortCardsBySuit(cards);
+        for (List<CardModel> sameSuitCards : cardsSortedBySuits) {
+            if (sameSuitCards.size() == 4) {
+                return sameSuitCards.get(0).getSuit();
+            }
+        }
+        return null;
+    }
+
+    public static List<List<CardModel>> sortCardsBySuit(List<CardModel> cardList) {
+        List<List<CardModel>> result = new ArrayList<>();
+        List<String> ranksAlreadyChecked = new ArrayList();
+
+        for (int i = 0; i < cardList.size(); i++) {
+            CardModel currentCard = cardList.get(i);
+            if (!ranksAlreadyChecked.contains(currentCard.getRank())) {
+
+                ranksAlreadyChecked.add(currentCard.getRank());
+                List<CardModel> foundFigure = new ArrayList<>();
+                foundFigure.add(currentCard);
+
+                for (int k = 0; k < cardList.size(); k++) {
+                    CardModel nextCard = cardList.get(k);
+
+                    if (!currentCard.equals(nextCard) && currentCard.getSuit().equals(nextCard.getSuit())) {
+                        foundFigure.add(nextCard);
+                    }
+                }
+
+                if (foundFigure.size() > 1) {
+                    result.add(foundFigure);
+                }
+            }
+        }
+        return result;
+
+    }
 }
